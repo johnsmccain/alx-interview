63f2851c58381b2daf405684a5495899ffdc6a7c
johnsmccain <johnsdanlami@gmail.com>
2022-12-21 23:36:23 +0100

    pascal trianglerx

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,21 +3,15 @@ def pascal_triangle(n):
     temp = []
     
     if n <= 0:
-        
         return []
 
     for i in range(1, n+1):
-        
         temp1 = [] 
         C = 1
 
         for j in range(1, i+1):
-
             temp1.append(C)
             C = C * (i - j) // j
-        
         temp.append(temp1)
          
     return temp
-
-#pascal_triangle(4)
