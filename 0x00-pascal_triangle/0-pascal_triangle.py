7b4004ab72e0d5f3ec4a6e495f539705e566e364
johnsmccain <johnsdanlami@gmail.com>
2022-12-22 23:41:05 +0100

    hot fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,28 @@
-''' Module: #0x00. Pascal's Triangle
-'''
+#!/usr/bin/python3
+"""
+Created on friday 2022
+
+@author: John Danlami
+"""
+
 
 def pascal_triangle(n):
     '''
-     pascal_triangle - that returns a list of 
-     lists of integers representing the Pascal’s 
+     pascal_triangle - that returns a list of
+     lists of integers representing the Pascal’s
      triangle of n:
-     @n: lists of integers
-     Return: []
+     @n: n will be always an integer
+     Returns: an empty list if n <= 0
     '''
-    temp = []
-    
     if n <= 0:
         return []
 
+    temp = []
     for i in range(1, n+1):
-        temp1 = [] 
+        temp1 = []
         C = 1
-
         for j in range(1, i+1):
             temp1.append(C)
             C = C * (i - j) // j
         temp.append(temp1)
-         
     return temp
